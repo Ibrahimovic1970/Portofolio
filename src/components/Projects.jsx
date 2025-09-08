6a7a03cc4932a79e72d242b842dcb5563e9e12b6
Ahmad Ibrahimovic <ahmad.ibrahimovic93@smk.belajar.id>
2025-09-08 16:39:06 +0700

    Update Projects

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -13,7 +13,7 @@ const Projects = () => {
 
     useEffect(() => {
         gsap.from(projectRefs.current, {
-            opacity: 0,
+            opacity: 2,
             y: 50,
             duration: 1.2,
             ease: 'power3.out',
